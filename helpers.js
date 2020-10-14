56f87ab7af7ac9751b8feb3e4e847dbcddb1b934
raghav3003 <34268524+raghav3003@users.noreply.github.com>
2020-10-14 13:12:25 +0530

    Create helpers.js

---
+++ helpers.js
@@ -0,0 +1,49 @@
+// Dependencies
+var crypto = require('crypto');
+var config = require('./config');
+
+var helpers = {};
+
+// Create a SHA256 hash
+helpers.hash = function(str) {
+  if (typeof(str) == 'string' && str.length > 0) {
+    var hash = crypto.createHmac('sha256', config.hashingSecret).update(str).digest('hex');
+    return hash;
+  } else {
+    return false;
+  }
+};
+
+// Parse a json string to an objecvt in all cases, without throwing
+helpers.parseJsonToObject = function(str) {
+  try {
+    var obj = JSON.parse(str);
+    return obj;
+  } catch(e) {
+    return {};
+  }
+};
+
+// Create a string of random alphanumeric characters, of a given length
+helpers.createRandomString = function(strLength) {
+  strLength = typeof(strLength) == 'number' && strLength > 0 ? strLength : false;
+  if (strLength) {
+    // Define all the possible characters that could go into the string
+    var possibleCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    // Start the final string
+    var str = '';
+    for (i = 1; i <= strLength; i++) {
+      // Get a random character from the possibleCharacters string
+      var randomCharacter = possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+      // Append this character to the final string
+      str += randomCharacter;
+    }
+    return str;
+
+  } else {
+    return false;
+  }
+}
+
+module.exports = helpers;
